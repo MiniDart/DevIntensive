249cdc3cf0692631f813d38bebe1f334f0efc767
Mini Dart <lu036@yandex.ru>
2016-06-28 18:48:35 +0300

    middle

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -2,20 +2,26 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Handler;
 import android.os.PersistableBundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TAG= ConstantManager.TAG_PREFIX+"Main Acrivity";
     private EditText mPhone, mEmail, mGitHub, mAbout, mVkProfile;
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -27,12 +33,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+        mCallImg=(ImageView) findViewById(R.id.call_img);
+        mCallImg.setOnClickListener(this);
+        mCoordinatorLayout=(CoordinatorLayout) findViewById(R.id.main_coordinator_container);
         if (savedInstanceState==null) {
-
+            showSnacbar("first launch");
+            showToast("first launch in toast");
 
             //first time run
         }
         else {
+            showSnacbar("second launch");
+            showToast("second launch in toast");
             //second time run
 
         }
@@ -77,9 +89,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.call_img:
+               /*showProgress();
+                runWithDelay();*/
+                break;
 
         }
     }
+    private void showSnacbar(String message){
+        Snackbar.make(mCoordinatorLayout,message,Snackbar.LENGTH_LONG).show();
+    }
+    /*public void runWithDelay(){
+        final Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideProgress();
+            }
+        },5000);
+    }*/
 
 }
 
